2d4b5ce3ef8128b5b7cd379e6503af43ff19860b
astranauta <astranauta311@gmail.com>
2017-04-09 01:15:45 -0500

    try that again

--- js/page-spells.js
+++ js/page-spells.js
@@ -284,6 +284,7 @@ function usespell (id) {
 			} else for (var i = 0; i < textlist.length; i++) {
 				if (!textlist[i]) continue;
 				if (curspell.level[0] !== "P") {
+					texthtml = texthtml + "<p>"+textlist[i].replace(/At Higher Levels(\:|\.) /g, "<strong>At Higher Levels:</strong> ").replace("This spell can be found in the Elemental Evil Player's Companion.","").replace(/^.*(Psychic Focus|Bestial Transformation|Atonement|Bless Water|Coming of Age|Dedication|Funeral Rite|Investiture|Marriage)+\./g,"<strong>$&</strong>").replace(/^.*\:/g,"<strong>$&</strong>")+"</p>";
 				}
 			}
 			$("tr#text").after("<tr class='text'><td colspan='6' class='text"+i+"'>"+texthtml+"</td></tr>");
