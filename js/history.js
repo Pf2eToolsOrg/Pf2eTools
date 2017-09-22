4f6bba904a9e86bd132ecbf5c10dd603ab76668e
James Campos <james.r.campos@gmail.com>
2017-09-21 23:46:34 -0700

    backgrounds

--- js/history.js
+++ js/history.js
@@ -1,6 +1,6 @@
 window.onhashchange = function hashchange(e) {
 	let $el = $("ul.list li[data-link='"+window.location.hash.split("#")[1]+"']:eq(0)");
 	loadhash($el.attr("id"));
-	document.title = decodeURIComponent($el.attr("data-name")).replace("%27","'") + " - 5etools";
+	document.title = decodeURIComponent($el.attr("data-link")).replace("%27","'") + " - 5etools";
 }
 
