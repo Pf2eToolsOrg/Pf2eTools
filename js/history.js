34de329cd72abe031e3fd84343fcc9a0f9ba6b76
Miles Long <cuck@me.com>
2017-09-30 20:10:39 +0100

    Removed debug code

--- js/history.js
+++ js/history.js
@@ -4,7 +4,6 @@ window.onhashchange = function hashchange(e) {
 	if (!e || !sub) {
 		const $el = $(`ul.list li[data-link='${link.toLowerCase()}']:eq(0)`);
 		loadhash($el.attr("id"));
-		console.log($el[0]);
 		document.title = decodeURIComponent($el.attr("title")) + " - 5etools";
 	}
 
