cc63b071c53626b088d97bef1ea4e726f44d4f0c
Miles Long <cuck@me.com>
2017-10-04 20:17:17 +0100

    WIP psionic loading

--- js/page-psionics.js
+++ js/page-psionics.js
@@ -1,16 +1,33 @@
 const STR_EMPTY = "";
+const STR_SLUG_DASH = "-";
+const STR_JOIN_MODE_LIST = ",";
+const STR_JOIN_MODE_TITLE_BRACKET_PART_LIST = "; ";
+const STR_JOIN_MODE_TITLE = " ";
 const STR_ABV_TYPE_TALENT = "T";
 const STR_ABV_TYPE_DISCIPLINE = "D";
 const STR_TYPE_TALENT = "Talent";
 const STR_TYPE_DISCIPLINE = "Discipline";
+const STR_ORDER_NONE = "None";
 
-const TMP_TYPE_ORDER = "{0} ({1})";
+const TMP_WINDOW_HASH = "#{0}";
+const TMP_TYPE_ORDER = "{0} {1}";
+const TMP_DISCIPLINE_TEXT = "{0}{1}{2}";
+const TMP_DESCRIPTION = "<p>{0}</p>";
+const TMP_FOCUS = "<p><span class='psi-focus-title'>Psycic Focus.</span> {0}</p>";
+const TMP_HIDDEN_MODE = "\"{0}\"";
+const TMP_MODE_TITLE = "<span class='psi-mode-title'>{0}.</span> ";
+const TMP_MODE_WITH_SUB_MODE = "{0}{1}";
+const TMP_MODE_TITLE_BRACKET_PART = "({0})";
+const TMP_MODE_TITLE_COST = "{0} psi";
+const TMP_MODE_TITLE_COST_RANGE = "{0}-{1}";
+const TMP_MODE_TITLE_CONCENTRATION = "conc., {0} {1}.";
 
 const ID_PSIONICS_LIST = "psionicsList";
 const ID_LIST_CONTAINER = "listContainer";
 const ID_SOURCE_FILTER = "sourceFilter";
 const ID_TYPE_FILTER = "typeFilter";
 const ID_ORDER_FILTER = "orderFilter";
+const ID_RESET_BUTTON = "reset";
 const ID_STATS_NAME = "name";
 const ID_STATS_ORDER_AND_TYPE = "orderAndType";
 const ID_STATS_DURATION = "duration";
@@ -22,6 +39,18 @@ const JSON_ITEM_TYPE = "type";
 const JSON_ITEM_ORDER = "order";
 const JSON_ITEM_TEXT = "text";
 const JSON_ITEM_DURATION = "duration";
+const JSON_ITEM_DESCRIPTION = "description";
+const JSON_ITEM_FOCUS = "focus";
+const JSON_ITEM_MODES = "modes";
+const JSON_ITEM_SUBMODES = "submodes";
+const JSON_ITEM_MODE_TITLE = "title";
+const JSON_ITEM_MODE_TEXT = "text";
+const JSON_ITEM_MODE_COST = "cost";
+const JSON_ITEM_MODE_COST_MIN = "min";
+const JSON_ITEM_MODE_COST_MAX = "max";
+const JSON_ITEM_MODE_CONCENTRATION = "concentration";
+const JSON_ITEM_MODE_CONCENTRATION_DURATION = "duration";
+const JSON_ITEM_MODE_CONCENTRATION_UNIT = "unit";
 
 const ELE_SPAN = "span";
 const ELE_LI = "li";
@@ -40,11 +69,16 @@ const CLS_COL1 = "col-xs-5";
 const CLS_COL2 = "col-xs-2";
 const CLS_COL3 = "col-xs-2";
 const CLS_COL4 = "col-xs-2";
+const CLS_HIDDEN = "hidden";
+const CLS_ORDER_NONE = "psi-list-order-none";
 
 const LIST_NAME = "name";
 const LIST_SOURCE = "source";
 const LIST_TYPE = "type";
 const LIST_ORDER = "order";
+const LIST_MODE_LIST = "mode-list";
+
+const EVENT_CLICK = "click";
 
 window.onload = function load() {
 	const TABLE_VIEW = document.getElementById(ID_PSIONICS_LIST);
@@ -53,7 +87,7 @@ window.onload = function load() {
 	populateListView(PSIONIC_LIST);
 	initFiltersAndSearch(PSIONIC_LIST);
 	initListLibrary();
-	selectFirstPsionic();
+	selectInitialPsionic();
 
 	function populateListView(psionicList) {
 		for (let i = 0; i < psionicList.length; ++i) {
@@ -64,7 +98,8 @@ window.onload = function load() {
 			listItem.appendChild(getSourceSpan(psionic));
 			listItem.appendChild(getTypeSpan(psionic));
 			listItem.appendChild(getOrderSpan(psionic));
-			listItem.addEventListener("click", listItemClick, false);
+			listItem.appendChild(getHiddenModeSpan(psionic));
+			listItem.addEventListener(EVENT_CLICK, listItemClick, false);
 
 			TABLE_VIEW.appendChild(listItem);
 		}
@@ -102,14 +137,34 @@ window.onload = function load() {
 			let span = document.createElement(ELE_SPAN);
 			span.classList.add(LIST_ORDER);
 			span.classList.add(CLS_COL4);
-			span.innerHTML = parse_psionicOrderToFull(psionic[JSON_ITEM_ORDER]);
+			let spanText = parse_psionicOrderToFull(psionic[JSON_ITEM_ORDER]);
+			if (spanText === STR_ORDER_NONE) {
+				span.classList.add(CLS_ORDER_NONE);
+			}
+			span.innerHTML = spanText;
 			return span;
+		}
+		function getHiddenModeSpan(psionic) {
+			let span = document.createElement(ELE_SPAN);
+			span.classList.add(LIST_MODE_LIST);
+			span.classList.add(CLS_HIDDEN);
+			span.innerHTML = getHiddenModeList(psionic);
+			return span;
+		}
+		function getHiddenModeList(psionic) {
+			let modeList = psionic[JSON_ITEM_MODES];
+			if (modeList === undefined) return STR_EMPTY;
+			let outArray = [];
+			for (let i = 0; i < modeList.length; ++i) {
+				outArray.push(TMP_HIDDEN_MODE.formatUnicorn(modeList[i][JSON_ITEM_MODE_TITLE]));
+			}
+			return outArray.join(STR_JOIN_MODE_LIST);
 		}
 		function listItemClick(event) {
 			loadDataLink(this);
 
 			function loadDataLink(listItem) {
-				window.location = "#" + listItem.getAttribute(ATB_DATA_LINK);
+				window.location = TMP_WINDOW_HASH.formatUnicorn(listItem.getAttribute(ATB_DATA_LINK));
 			}
 		}
 	}
@@ -122,6 +177,7 @@ window.onload = function load() {
 
 		populateFilterSets();
 		initFilters();
+		initResetButton();
 
 		function populateFilterSets() {
 			for (let i = 0; i < PSIONIC_LIST.length; ++i) {
@@ -138,7 +194,7 @@ window.onload = function load() {
 			}
 		}
 
-		function initFilters() {
+		function initFilters() { // TODO
 			addOptions();
 
 			function addOptions() {
@@ -171,16 +227,25 @@ window.onload = function load() {
 					return option;
 				}
 			}
+
+			function stringToSlug(str) {
+				return str.toLowerCase().replace(/[^\w ]+/g, STR_EMPTY).replace(/ +/g, STR_SLUG_DASH);
+			}
 		}
 
-		function stringToSlug(str) {
-			return str.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+		function initResetButton() { // TODO
+			const RESET_BUTTON = document.getElementById(ID_RESET_BUTTON);
+			RESET_BUTTON.addEventListener(EVENT_CLICK, resetButtonClick, false);
+
+			function resetButtonClick(event) {
+
+			}
 		}
 	}
 
 	function initListLibrary() {
 		let options = {
-			valueNames: [LIST_NAME, LIST_SOURCE, LIST_TYPE, LIST_ORDER],
+			valueNames: [LIST_NAME, LIST_SOURCE, LIST_TYPE, LIST_ORDER, LIST_MODE_LIST],
 			listClass: CLS_PSIONICS,
 			sortFunction: listSort
 		};
@@ -205,10 +270,14 @@ window.onload = function load() {
 		}
 	}
 
-	function selectFirstPsionic() {
-		let listItems = TABLE_VIEW.getElementsByTagName(ELE_LI);
-		if (listItems.length > 0) {
-			listItems[0].click();
+	function selectInitialPsionic() {
+		if (window.location.hash.length === 0) {
+			let listItems = TABLE_VIEW.getElementsByTagName(ELE_LI);
+			if (listItems.length > 0) {
+				listItems[0].click();
+			}
+		} else {
+			window.onhashchange();
 		}
 	}
 };
@@ -231,9 +300,58 @@ function loadhash (jsonIndex) {
 		STATS_DURATION.innerHTML = STR_EMPTY;
 	}
 	function loadDiscipline() {
-		STATS_ORDER_AND_TYPE.innerHTML = TMP_TYPE_ORDER.formatUnicorn(parse_psionicTypeToFull(selectedPsionic[JSON_ITEM_TYPE]), selectedPsionic[JSON_ITEM_ORDER]);
-
+		STATS_ORDER_AND_TYPE.innerHTML = TMP_TYPE_ORDER.formatUnicorn(selectedPsionic[JSON_ITEM_ORDER], parse_psionicTypeToFull(selectedPsionic[JSON_ITEM_TYPE]));
+		STATS_TEXT.innerHTML = getTextString();
 		STATS_DURATION.innerHTML = getDurationString();
+
+		function getTextString() {
+			let modeStringArray = [];
+			for (let i = 0; i < selectedPsionic[JSON_ITEM_MODES].length; ++i) {
+				modeStringArray.push(getModeString(i));
+			}
+
+			return TMP_DISCIPLINE_TEXT.formatUnicorn(getDescriptionString(), getFocusString(), modeStringArray.join(STR_EMPTY));
+		}
+		function getDescriptionString() {
+			return TMP_DESCRIPTION.formatUnicorn(selectedPsionic[JSON_ITEM_DESCRIPTION]);
+		}
+		function getFocusString() {
+			return TMP_FOCUS.formatUnicorn(selectedPsionic[JSON_ITEM_FOCUS]);
+		}
+		function getModeString(modeIndex) {
+			let modeString = utils_combineText(selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_TEXT], ELE_P, getModeTitle());
+			if (selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_SUBMODES] === undefined) return modeString;
+			let subModeString = ""; // TODO
+			return TMP_MODE_WITH_SUB_MODE.formatUnicorn(modeString, subModeString);
+
+			function getModeTitle() {
+				let modeTitleArray = [];
+				modeTitleArray.push(selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_TITLE]);
+				let bracketPart = getModeTitleBracketPart();
+				if (bracketPart !== null) modeTitleArray.push(bracketPart);
+				return TMP_MODE_TITLE.formatUnicorn(modeTitleArray.join(STR_JOIN_MODE_TITLE));
+
+				function getModeTitleBracketPart() {
+					let modeTitleBracketArray = [];
+
+					if (selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_COST]) modeTitleBracketArray.push(getModeTitleCost());
+					if (selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_CONCENTRATION]) modeTitleBracketArray.push(getModeTitleConcentration());
+
+					if (modeTitleBracketArray.length === 0) return null;
+					return TMP_MODE_TITLE_BRACKET_PART.formatUnicorn(modeTitleBracketArray.join(STR_JOIN_MODE_TITLE_BRACKET_PART_LIST));
+
+					function getModeTitleCost() {
+						let costMin = selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_COST][JSON_ITEM_MODE_COST_MIN];
+						let costMax = selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_COST][JSON_ITEM_MODE_COST_MAX];
+						let costString = costMin === costMax ? costMin : TMP_MODE_TITLE_COST_RANGE.formatUnicorn(costMin, costMax);
+						return TMP_MODE_TITLE_COST.formatUnicorn(costString)
+					}
+					function getModeTitleConcentration() {
+						return TMP_MODE_TITLE_CONCENTRATION.formatUnicorn(selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_CONCENTRATION][JSON_ITEM_MODE_CONCENTRATION_DURATION], selectedPsionic[JSON_ITEM_MODES][modeIndex][JSON_ITEM_MODE_CONCENTRATION][JSON_ITEM_MODE_CONCENTRATION_UNIT])
+					}
+				}
+			}
+		}
 	}
 	function getDurationString() {
 		let duration = selectedPsionic[JSON_ITEM_DURATION];
@@ -252,5 +370,5 @@ function parse_psionicTypeToFull(type) {
 	else return type;
 }
 function parse_psionicOrderToFull(order) {
-	return order === undefined ? STR_EMPTY : order;
+	return order === undefined ? STR_ORDER_NONE : order;
 }
