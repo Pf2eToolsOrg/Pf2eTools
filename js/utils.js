2f4778476775b2aff0a9534bee25239e1505b3cf
Xavier Dane <TheGiddyLimit@users.noreply.github.com>
2017-10-09 17:01:04 +0100

    WIP fixes

--- js/utils.js
+++ js/utils.js
@@ -75,7 +75,7 @@ String.prototype.uppercaseFirst = String.prototype.uppercaseFirst ||
 		let str = this.toString();
 		if (str.length === 0) return str;
 		if (str.length === 1) return str.charAt(0).toUpperCase();
-		return str.charAt(0).toUpperCase() + str.slice(1);;
+		return str.charAt(0).toUpperCase() + str.slice(1);
 	};
 
 // TEXT COMBINING ======================================================================================================
