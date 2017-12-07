c346a496e3fd1e14806927d176b558c55a8f6574
Miles Long <TheGiddyLimit@users.noreply.github.com>
2017-12-07 12:13:20 +0000

    fix(validation) update for new format

--- test/alltests.js
+++ test/alltests.js
@@ -11,9 +11,22 @@ const results = [];
 let errors = [];
 
 // Loop through each non-helper schema and push all validation results
-fs.readdirSync("./test/schema").forEach(file => {
+fs.readdirSync("./test/schema")
+	.filter(file => file.endsWith(".json")) // ignore directories
+	.forEach(file => {
 	if (file !== helperFile) results.push(validator.validate(require(`../data/${file}`), require(`./schema/${file}`), {nestedErrors: true}));
-})
+});
+
+// Custom handling for the new spells data layout TODO expand to cover everything
+fs.readdirSync(`./data/spells`)
+	.filter(file => file.startsWith("spells") || file.startsWith("roll20"))
+	.forEach(file => {
+		if (file.startsWith("spells")) {
+			results.push(validator.validate(require(`../data/spells/${file}`), require(`./schema/spells/spells.json`), {nestedErrors: true}));
+		} else {
+			results.push(validator.validate(require(`../data/spells/${file}`), require(`./schema/spells/roll20.json`), {nestedErrors: true}));
+		}
+});
 
 results.forEach( (result) => {
 	if (!result.valid) errors = errors.concat(result.errors);
