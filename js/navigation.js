aab774db5dbb2a85e6e5cd371125ff5a8adacc54
TheGiddyLimit <TheGiddyLimit@5etools.com>
2017-12-11 18:08:21 +0000

    chore(nav) lint

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,18 @@
+"use strict"
+
 window.addEventListener('load', navigation, false);
 window.addEventListener('load', currentPage, false);
 
 function currentPage(){
-	var currentPage = window.location.pathname;
+	let currentPage = window.location.pathname;
 	currentPage = currentPage.slice(1);
 
-	var current = document.querySelectorAll('a[href="'+currentPage+'"]');
+	const current = document.querySelectorAll('a[href="'+currentPage+'"]');
 	current[0].parentNode.className = 'active';
 
-	var parent = current[0].parentNode.parentNode.parentNode;
+	const parent = current[0].parentNode.parentNode.parentNode;
 	if(parent.tagName === 'LI'){
-		var dropdown = document.getElementById(parent.id);
+		const dropdown = document.getElementById(parent.id);
 		dropdown.className = 'dropdown active';
 	}
 }
@@ -60,7 +62,7 @@ function navigation() {
 	LISwitcher('navbar','daynightMode','nightModeToggle','#','styleSwitcher.toggleActiveStyleSheet(); return false;');
 
 	function A(append_to_id, _id, _class, _datatoggle, _href, _role, _ariahaspop, _ariaexpanded, _text){
-		var a = document.createElement('a');
+		const a = document.createElement('a');
 		a.id = _id;
 		a.className = _class;
 		a.setAttribute('data-toggle',_datatoggle);
@@ -70,72 +72,72 @@ function navigation() {
 		a.setAttribute('aria-expanded', _ariaexpanded);
 		a.innerHTML  = _text;
 
-		var appendTo = document.getElementById(append_to_id);
+		const appendTo = document.getElementById(append_to_id);
 		appendTo.appendChild(a);
 	}
 
 	function UL(append_to_id, ul_id, _class) {
-		var ul = document.createElement('ul');
+		const ul = document.createElement('ul');
 		ul.id = ul_id;
 		ul.className = _class;
 
-		var appendTo = document.getElementById(append_to_id);
+		const appendTo = document.getElementById(append_to_id);
 		appendTo.appendChild(ul);
 	}
 
 	function LI(append_to_id, a_href, a_text) {
-		var a = document.createElement('a');
+		const a = document.createElement('a');
 		a.href = a_href;
 		a.innerHTML = a_text;
 
-		var li = document.createElement('li');
+		const li = document.createElement('li');
 		li.id = a_text.toLowerCase().replace(/\s+/g, '');
 		li.setAttribute('role','presentation');
 		li.appendChild(a);
 
-		var appendTo = document.getElementById(append_to_id);
+		const appendTo = document.getElementById(append_to_id);
 		appendTo.appendChild(li);
 	}
 
 	function LISpecial(append_to_id, a_href, a_text, a_target, a_title) {
-		var a = document.createElement('a');
+		const a = document.createElement('a');
 		a.href = a_href;
 		a.setAttribute('target',a_target);
 		a.setAttribute('title',a_title);
 		a.innerHTML = a_text;
 
-		var li = document.createElement('li');
+		const li = document.createElement('li');
 		li.id = a_text.toLowerCase().replace(/\s+/g, '');
 		li.setAttribute('role','presentation');
 		li.appendChild(a);
 
-		var appendTo = document.getElementById(append_to_id);
+		const appendTo = document.getElementById(append_to_id);
 		appendTo.appendChild(li);
 	}
 
 	function LIDropdown(append_to_id, li_id, _class) {
-		var li = document.createElement('li');
+		const li = document.createElement('li');
 		li.id = li_id;
 		li.setAttribute('role','presentation');
 		li.className = _class;
 
-		var appendTo = document.getElementById(append_to_id);
+		const appendTo = document.getElementById(append_to_id);
 		appendTo.appendChild(li);
 	}
 	
 	function LISwitcher(append_to_id, li_id, a_class, a_href, a_onclick){
-		var a = document.createElement('a');
+		const a = document.createElement('a');
 		a.href = a_href;
 		a.className = a_class;
 		a.setAttribute('onclick',a_onclick);
 		a.innerHTML = styleSwitcher.getActiveStyleSheet() === StyleSwitcher.STYLE_DAY ? "Night Mode" : "Day Mode";
 		
-		var li = document.createElement('li');
+		const li = document.createElement('li');
 		li.id = li_id;
 		li.setAttribute('role','presentation');
 		li.appendChild(a);
 		
-		var appendTo = document.getElementById(append_to_id);
+		const appendTo = document.getElementById(append_to_id);
 		appendTo.appendChild(li);
 	}
 }
