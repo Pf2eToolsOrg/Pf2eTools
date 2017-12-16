d437c63517ac5da2d833994f3f7e621d78ef1d8b
TheGiddyLimit <TheGiddyLimit@5etools.com>
2017-12-16 13:55:49 +0000

    feat(adventures) improve inset CSS; add readaloud CSS

--- js/entryrender.js
+++ js/entryrender.js
@@ -92,19 +92,20 @@ function EntryRenderer () {
 					}
 					textStack.push(`</${this.wrapperTag}>`);
 					break;
-				case "invocation":
-					handleInvocation(this);
-					break;
-				case "patron":
-					handlePatron(this);
-					break;
-				case "inset":
-					textStack.push(`<${this.wrapperTag} class="statsBlockInset">`);
+				case "insetReadaloud":
+					textStack.push(`<${this.wrapperTag} class="statsBlockInsetReadaloud">`);
 					if (typeof entry.name !== 'undefined') textStack.push(`<span class="entry-title">${entry.name}</span>`);
 					for (let i = 0; i < entry.entries.length; i++) {
 						this.recursiveEntryRender(entry.entries[i], textStack, 2, "<p>", "</p>");
 					}
 					textStack.push(`</${this.wrapperTag}>`);
+					break;
+
+				case "invocation":
+					handleInvocation(this);
+					break;
+				case "patron":
+					handlePatron(this);
 					break;
 
 				// block
