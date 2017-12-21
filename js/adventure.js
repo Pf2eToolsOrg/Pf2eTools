885b67fad4e28a803f9f507170e42d68f4f6c9fb
TheGiddyLimit <TheGiddyLimit@users.noreply.github.com>
2017-12-21 23:20:15 +0000

    Adventure (#20)
    * feat(renderer) add sidebar/insert handling
    
    * feat(renderer) fix layout; use for class features
    
    * fix(history) cleanup
    
    * feat(adventures) WIP
    
    * feat(adventures) adventures WIP
    
    * fix(variants) typo
    
    * fix(encounters) lint
    
    * feat(adventures) WIP
    
    * fix(adventures) remove leading periods
    
    * feat(adventures) add temp rendering for TODO links
    
    * fix(adventures) remove duplication
    
    * feat(adventures) WIP data improvements
    
    * feat(adventures) improve inset CSS; add readaloud CSS
    
    * feat(adventures) lint
    
    * feat(adventures) \s fix; add missing links
    
    * feat(adventures) images
    
    * feat(adventures) add image titles
    
    * feat(renderer) add image titles
    
    * feat(adventure) add image captions
    
    * feat(adventure) fix null
    
    * feat(adventure) fix maps; add map images
    
    * feat(adventure) remove encounter/parcel
    
    * feat(adventure) remove junk links
    
    * feat(adventure) scroll to top of page on changing Part
    
    * feat(adventures) remove links
    
    * feat(adventures) remove links
    
    * feat(adventures) add NPC details
    
    * feat(adventures) fix NPC links
    
    * feat(adventures) fix quests
    
    * feat(adventures) better layout
    
    * feat(adventures) creature links
    
    * feat(items) fix and improve list hiding
    
    * feat(adventure) add item links
    
    * fix(items) add missing LMoP items
    
    * feat(adventure) more items
    
    * feat(adventure) add tables
    
    * feat(adventure) fix column ordering
    
    * feat(adventure) fix ranges
    
    * feat(adventure) more item/creature links
    
    * feat(adventure) spell links
    
    * feat(adventure) remove appendix references
    
    * feat(adventure) add spell scroll links
    
    * CI: Notify is now after deployment. [SKIP CI]
    
    * CI: Better config. [SKIP CI]
    
    * feat(adventures) more item links
    
    * feat(adventures) improve UX
    
    * feat(adventures) move punctuation outside links
    
    * feat(adventures) more links
    
    * feat(adventures) lint
    
    * fix(items) restore

---
+++ js/adventure.js
@@ -0,0 +1,96 @@
+"use strict";
+
+let renderArea;
+
+let adventures;
+
+const TABLE_START = `<tr><th class="border" colspan="6"></th></tr>`;
+const TABLE_END = `<tr><th class="border" colspan="6"></th></tr>`;
+
+window.onload = function load () {
+	renderArea = $(`#stats`);
+
+	renderArea.append(TABLE_START);
+	renderArea.append(`<tr><td colspan="6" class="initial-message">Select an adventure to begin</td></tr>`);
+	renderArea.append(TABLE_END);
+
+	loadJSON(CONTENTS_URL, onJsonLoad);
+};
+
+function onJsonLoad (data) {
+	adventures = data.adventure;
+
+	const adventuresList = $("ul.contents");
+	let tempString = "";
+	for (let i = 0; i < adventures.length; i++) {
+		const adv = adventures[i];
+
+		tempString +=
+			`<li>
+				<a id="${i}" href='#${adv.id},0' title='${adv.name}'>
+					<span class='name'>${adv.name}</span> 
+				</a>
+				${makeContentsBlock(adv, false, true)}
+			</li>`;
+	}
+	adventuresList.append(tempString);
+
+	const list = new List("listcontainer", {
+		valueNames: ['name'],
+		listClass: "contents"
+	});
+
+	window.onhashchange = hashChange;
+	if (window.location.hash.length) {
+		hashChange();
+	}
+}
+
+// custom loading for this page, so it can serve multiple sources
+function hashChange () {
+	const [advId, ...hashParts] = window.location.hash.slice(1).split(HASH_PART_SEP);
+	const fromIndex = adventures.filter(adv => adv.id === advId);
+	if (fromIndex.length) {
+		document.title = `${fromIndex[0].name} - 5etools`;
+		$(`.adv-header`).html(fromIndex[0].name);
+		$(`.adv-message`).html("Select a chapter on the left, and browse the content on the right");
+		loadAdventure(fromIndex[0], advId, hashParts);
+	} else {
+		throw new Error("No adventure with ID: " + advId);
+	}
+}
+
+function loadAdventure (fromIndex, advId, hashParts) {
+	loadJSON(`data/adventures/${advId}.json`, function (data) {
+		onAdventureLoad(data, fromIndex, advId, hashParts);
+	});
+}
+
+const renderer = new EntryRenderer();
+
+const curRender = {
+	chapter: -1
+};
+function onAdventureLoad (data, fromIndex, advId, hashParts) {
+	let chapter = 0;
+	let scrollTo;
+	if (hashParts && hashParts.length > 0) chapter = Number(hashParts[0]);
+	if (hashParts && hashParts.length > 1) {
+		scrollTo = $(`[href="#${advId},${chapter},${hashParts[1]}"]`).data("header");
+	}
+
+	if (curRender.chapter !== chapter) {
+		curRender.chapter = chapter;
+		renderArea.html("");
+
+		renderArea.append(TABLE_START);
+		const textStack = [];
+		renderer.recursiveEntryRender(data[chapter], textStack);
+		renderArea.append(`<tr class='text'><td colspan='6'>${textStack.join("")}</td></tr>`);
+		renderArea.append(TABLE_END);
+
+		setTimeout(() => {
+			scrollClick(scrollTo);
+		}, 75)
+	}
+}
