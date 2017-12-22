abc75160be4f1e008b23181158aca1c57f87b954
MrLabRat <mrlabrat@5etools.com>
2017-12-22 11:33:39 +0000

    Removed operators from lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,7 +72,6 @@ module.exports = {
 		"no-mixed-operators": ["error", {
 			"groups": [
 				["==", "!=", "===", "!==", ">", ">=", "<", "<="],
-				["&&", "||"],
 				["in", "instanceof"]
 			],
 			"allowSamePrecedence": true
