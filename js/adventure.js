cd5b0f8eb0616d468e9027d2c2decfe5543d43a5
TheGiddyLimit <TheGiddyLimit@5etools.com>
2017-12-23 13:35:48 +0000

    fix(adventures) fix JSON layout

--- js/adventure.js
+++ js/adventure.js
@@ -61,8 +61,8 @@ function hashChange () {
 }
 
 function loadAdventure (fromIndex, advId, hashParts) {
-	loadJSON(`data/adventures/${advId}.json`, function (data) {
-		onAdventureLoad(data, fromIndex, advId, hashParts);
+	loadJSON(`data/adventures/adventure-${advId}.json`, function (data) {
+		onAdventureLoad(data.data, fromIndex, advId, hashParts);
 	});
 }
 
