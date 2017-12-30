955a6acb976b4edbfc5b2b39dd4b4210f687e1c4
DBAWiseMan <DBAWiseMan@users.noreply.github.com>
2017-12-30 14:47:04 +0000

    chore(JSON) whitespace tidy-up and PNG reconciliation
    Replaced double spaces with tabs for indentation and replaced double spaces with single spaces for "normal text".
    Added reconciliation of PNG image files vs. bestiary expectations to `alltests.js`.

--- test/alltests.js
+++ test/alltests.js
@@ -3,23 +3,23 @@ const Validator = require('jsonschema').Validator;
 const validator = new Validator();
 const helperFile = "entry.json";
 validator.addSchema(require(`./schema/${helperFile}`), "/Entry");
-
 const TESTS_PASSED = 0;
 const TESTS_FAILED = 1;
-
-const results = [];
+var results = [];
+const expected = [];
+const existing = [];
 
 // Loop through each non-helper schema and push all validation results.
 fs.readdirSync("./test/schema")
 	.filter(file => file.endsWith(".json")) // ignore directories
 	.forEach(file => {
-	if (file !== helperFile) {
-		console.log(`Testing data/${file}`.padEnd(50), `against schema/${file}`);
-		const result = validator.validate(require(`../data/${file}`), require(`./schema/${file}`));
-		checkHandleError(result);
-		results.push(result);
-	}
-});
+		if (file !== helperFile) {
+			console.log(`Testing data/${file}`.padEnd(50), `against schema/${file}`);
+			const result = validator.validate(require(`../data/${file}`), require(`./schema/${file}`));
+			checkHandleError(result);
+			results.push(result);
+		}
+	});
 
 fs.readdirSync(`./test/schema`)
 	.filter(category => !category.endsWith(".json")) // only directories
@@ -36,15 +36,47 @@ fs.readdirSync(`./test/schema`)
 		})
 	});
 
-// Reporting
 console.log("All schema tests passed.");
+
+// Loop through each bestiary JSON file push the list of expected PNG files.
+fs.readdirSync("./data/bestiary")
+	.filter(file => file.startsWith("bestiary") && file.endsWith(".json"))
+	.forEach(file => {
+		const result = JSON.parse(fs.readFileSync(`./data/bestiary/${file}`));
+		for (let i = 0; i < result.monster.length; i++) expected.push(`${result.monster[i].source}/${result.monster[i].name.replace(/"/g, "")}.png`);
+	});
+
+// Loop through each bestiary-related img directory and push the list of files in each.
+fs.readdirSync("./img")
+	.filter(file => !file.endsWith(".png"))
+	.forEach(dir => {
+		if (dir !== "adventure" && dir !== "variantrules") {
+			fs.readdirSync(`./img/${dir}`).forEach(file => {
+				existing.push(`${dir.replace("(", "").replace(")", "")}/${file}`);
+			})
+		}
+	});
+
+results = [];
+expected.forEach(function (i) {
+	if (existing.indexOf(i) === -1) results.push(`${i} is missing`);
+});
+existing.forEach(function (i) {
+	if (expected.indexOf(i) === -1) results.push(`${i} is extra`);
+});
+results.sort(function (a, b) {
+	return a.toLowerCase().localeCompare(b.toLowerCase());
+}).forEach(function (i) {
+	console.log(i);
+});
+
 process.exit(TESTS_PASSED);
 
 /**
  * Fail-fast
  * @param result a result to check
  */
-function checkHandleError(result) {
+function checkHandleError (result) {
 	if (!result.valid) {
 		console.error(JSON.stringify(result.errors, null, 2));
 		console.warn(`Tests failed`);
