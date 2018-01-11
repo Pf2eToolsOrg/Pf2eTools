0b996d54a2a527a7e8796b3025f5b7ea27af4c64
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-01-11 23:30:18 +0000

    feat(gods) finalise gods page

--- js/gods.js
+++ js/gods.js
@@ -2,14 +2,32 @@
 
 const JSON_URL = "data/gods.json";
 
-
 window.onload = function load () {
 	DataUtil.loadJSON(JSON_URL, onJsonLoad);
 };
 
 function parseAlignmentToFull (alignment) {
-	// TODO
+	alignment = alignment.toUpperCase();
+	switch (alignment) {
+		case "L":
+			return "Lawful";
+		case "N":
+			return "Neutral";
+		case "C":
+			return "Chaotic";
+		case "G":
+			return "Good";
+		case "E":
+			return "Evil";
+	}
 	return alignment;
+}
+
+function alignSort (a, b) {
+	const first = ["L", "C"];
+	if (a === b) return 0;
+	if (first.includes(a)) return -1;
+	return 1;
 }
 
 let godsList;
@@ -18,20 +36,24 @@ function onJsonLoad (data) {
 	godsList = data.god;
 
 	const alignmentFilter = new Filter({
-		header: "Alignment", // TODO prefill
+		header: "Alignment",
+		items: ["C", "E", "G", "L", "N"],
 		displayFn: parseAlignmentToFull
 	});
 	const pantheonFilter = new Filter({
-		header: "Pantheon" // TODO prefill
+		header: "Pantheon",
+		items: ["Celtic", "Dragonlance", "Eberron", "Egyptian", "Forgotten Realms", "Greek", "Greyhawk", "Nonhuman", "Norse"]
 	});
 	const domainFilter = new Filter({
-		header: "Domain" // TODO prefill
+		header: "Domain",
+		items: ["Death", "Knowledge", "Life", "Light", "Nature", "Tempest", "Trickery", "War"]
 	});
 
 	const filterBox = initFilterBox(alignmentFilter, pantheonFilter, domainFilter);
 
 	let tempString = "";
 	godsList.forEach((g, i) => {
+		g.alignment.sort(alignSort);
 		g.domains.sort(ascSort);
 
 		tempString += `
@@ -39,23 +61,13 @@ function onJsonLoad (data) {
 				<a id="${i}" href="#${UrlUtil.autoEncodeHash(g)}" title="${g.name}">
 					<span class="name col-xs-4">${g.name}</span>
 					<span class="pantheon col-xs-2 text-align-center">${g.pantheon}</span>
-					<span class="alignment col-xs-2 text-align-center">${g.alignment}</span>
+					<span class="alignment col-xs-2 text-align-center">${g.alignment.join("")}</span>
 					<span class="domains col-xs-4">${g.domains.join(", ")}</span>
 				</a>
 			</li>
 		`;
-
-		// populate filters
-		alignmentFilter.addIfAbsent(g.alignment);
-		pantheonFilter.addIfAbsent(g.pantheon);
-		g.domains.forEach(d => domainFilter.addIfAbsent(d))
 	});
 	$(`#godsList`).append(tempString);
-	// sort filters
-	// FIXME remove when they're static
-	alignmentFilter.items.sort(ascSort);
-	pantheonFilter.items.sort(ascSort);
-	domainFilter.items.sort(ascSort);
 
 	const list = search({
 		valueNames: ["name", "pantheon", "alignment", "domains", "symbol"],
@@ -109,12 +121,11 @@ function loadhash (jsonIndex) {
 	const sourceFull = Parser.sourceJsonToFull(god.source);
 
 	const $content = $(`#pagecontent`);
-	debugger
 	$content.html(`
 		<tr><th class="border" colspan="6"></th></tr>
 		<tr><th class="name" colspan="6"><span class="stats-name">${god.name}</span><span class="stats-source source${god.source}" title="${sourceFull}">${Parser.sourceJsonToAbv(god.source)}</span></th></tr>
-		<tr><td colspan="6"><span class="bold">Pantheon: ${god.pantheon}</span></td></tr>
-		<tr><td colspan="6"><span class="bold">Alignment: ${parseAlignmentToFull(god.alignment)}</span></td></tr>
+		<tr><td colspan="6"><span class="bold">Pantheon: </span>${god.pantheon}</td></tr>
+		<tr><td colspan="6"><span class="bold">Alignment: </span>${god.alignment.map(a => parseAlignmentToFull(a)).join(" ")}</td></tr>
 		<tr><td colspan="6"><span class="bold">Domains: </span>${god.domains.join(", ")}</td></tr>
 		<tr><td colspan="6"><span class="bold">Symbol: </span>${god.symbol}</td></tr>
 		${god.page ? `<td colspan=6><b>Source: </b> <i>${sourceFull}</i>, page ${god.page}</td>` : ""}
