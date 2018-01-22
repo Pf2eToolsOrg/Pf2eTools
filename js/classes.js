34731dc0d029c43ab3878d4973e6080f4a340b1d
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-01-22 22:15:04 +0000

    feat(homebrew) add download button

--- js/classes.js
+++ js/classes.js
@@ -680,7 +680,10 @@ function manageBrew () {
 				const $btnDel = $(`<button class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash""></span></button>`).on("click", () => {
 					deleteFn(j.uniqueId);
 				});
-				$brewList.append($(`<p>`).append($btnDel).append(`&nbsp; <i>${type}${prop === "subclass" ? ` (${j.class})` : ""}:</i> <b>${j.name} ${j.version ? ` (v${j.version})` : ""}</b> by ${j.authors ? j.authors.join(", ") : "Anonymous"}. ${j.url ? `<a href="${j.url}" target="_blank">Source.</a>` : ""}`));
+				const $btnExport = $(`<button class="btn btn-default btn-sm"><span class="glyphicon glyphicon-download-alt"></span></button>`).on("click", () => {
+					DataUtil.userDownload(j.name, JSON.stringify(j, null, "\t"));
+				});
+				$brewList.append($(`<p>`).append($btnDel).append(" ").append($btnExport).append(`&nbsp; <i>${type}${prop === "subclass" ? ` (${j.class})` : ""}:</i> <b>${j.name} ${j.version ? ` (v${j.version})` : ""}</b> by ${j.authors ? j.authors.join(", ") : "Anonymous"}. ${j.url ? `<a href="${j.url}" target="_blank">Source.</a>` : ""}`));
 			});
 		}
 
