9bb27412a30056ec4ecab91b603c1214f03fc1fb
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-07-02 15:01:43 +0100

    fix(lifegen): improve print styles
    (cherry picked from commit 940e61d)

--- js/lifegen.js
+++ js/lifegen.js
@@ -641,7 +641,7 @@ function sectFamily () {
 	$family.empty();
 	$family.append(`<b>Family:</b> ${_getFromTable(FAMILY, RNG(100)).result}<br>`);
 	let famIndex = 1;
-	const $btnSuppFam = $(`<button class="btn btn-xs btn-default btn-supp-fam"></button>`).on("click", () => {
+	const $btnSuppFam = $(`<button class="btn btn-xs btn-default btn-supp-fam noprint"></button>`).on("click", () => {
 		const supDetails = getPersonDetails();
 		const $wrpRes = $(`<div class="output-wrp-border"/>`);
 		$wrpRes.append(`<h5>Family Member Roll ${famIndex++}</h5>`);
