c2ccdd2adeb084c664073af1369757313aaa7113
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-07-12 20:32:50 +0100

    fix(multisource): fix promise ordering

--- js/multisource.js
+++ js/multisource.js
@@ -7,84 +7,90 @@ const JSON_SRC_INDEX = "index.json";
  * @param jsonListName the name of the root JSON property for the list of data
  * @param pageInitFn function to be run once the index has loaded, should accept an object of src:URL mappings
  * @param dataFn function to be run when all data has been loaded, should accept a list of objects custom to the page
- * * @param cbOpt optional callback to be run after dataFn, but before page history/etc is init'd
+ * @param pOptional optional promise to be run after dataFn, but before page history/etc is init'd
  * (e.g. spell data objects for the spell page) which were found in the `jsonListName` list
  */
-function multisourceLoad (jsonDir, jsonListName, pageInitFn, dataFn, cbOpt) {
-	// load the index
-	DataUtil.loadJSON(jsonDir + JSON_SRC_INDEX).then(function (index) {
-		_onIndexLoad(index, jsonDir, jsonListName, pageInitFn, dataFn, cbOpt)
+function multisourceLoad (jsonDir, jsonListName, pageInitFn, dataFn, pOptional) {
+	return new Promise(resolve => {
+		// load the index
+		DataUtil.loadJSON(jsonDir + JSON_SRC_INDEX)
+			.then((index) => _onIndexLoad(index, jsonDir, jsonListName, pageInitFn, dataFn, pOptional))
+			.then(resolve);
 	});
 }
 
 let loadedSources;
-function _onIndexLoad (src2UrlMap, jsonDir, dataProp, pageInitFn, addFn, cbOpt) {
-	// track loaded sources
-	loadedSources = {};
-	Object.keys(src2UrlMap).forEach(src => loadedSources[src] = {url: jsonDir + src2UrlMap[src], loaded: false});
+function _onIndexLoad (src2UrlMap, jsonDir, dataProp, pageInitFn, addFn, pOptional) {
+	return new Promise(resolve => {
+		// track loaded sources
+		loadedSources = {};
+		Object.keys(src2UrlMap).forEach(src => loadedSources[src] = {url: jsonDir + src2UrlMap[src], loaded: false});
 
-	// collect a list of sources to load
-	const sources = Object.keys(src2UrlMap);
-	const defaultSel = sources.filter(s => defaultSourceSelFn(s));
-	const userSel = [...new Set((FilterBox.getSelectedSources() || []).concat((ListUtil.getSelectedSources() || [])))];
+		// collect a list of sources to load
+		const sources = Object.keys(src2UrlMap);
+		const defaultSel = sources.filter(s => defaultSourceSelFn(s));
+		const userSel = [...new Set((FilterBox.getSelectedSources() || []).concat((ListUtil.getSelectedSources() || [])))];
 
-	const allSources = [];
+		const allSources = [];
 
-	// add any sources from the user's saved filters, provided they have URLs and haven't already been added
-	if (userSel) {
-		userSel
-			.filter(src => src2UrlMap[src])
-			.filter(src => $.inArray(src, allSources) === -1)
-			.forEach(src => allSources.push(src));
-	}
-	// if there's no saved filters, load the defaults
-	if (allSources.length === 0) {
-		// remove any sources that don't have URLs
-		defaultSel.filter(src => src2UrlMap[src]).forEach(src => allSources.push(src));
-	}
-
-	// add source from the current hash, if there is one
-	if (window.location.hash.length) {
-		const [link, ...sub] = History._getHashParts();
-		const src = link.split(HASH_LIST_SEP)[1];
-		const hashSrcs = {};
-		sources.forEach(src => hashSrcs[UrlUtil.encodeForHash(src)] = src);
-		const mapped = hashSrcs[src];
-		if (mapped && $.inArray(mapped, allSources) === -1) {
-			allSources.push(mapped);
+		// add any sources from the user's saved filters, provided they have URLs and haven't already been added
+		if (userSel) {
+			userSel
+				.filter(src => src2UrlMap[src])
+				.filter(src => $.inArray(src, allSources) === -1)
+				.forEach(src => allSources.push(src));
 		}
-	}
+		// if there's no saved filters, load the defaults
+		if (allSources.length === 0) {
+			// remove any sources that don't have URLs
+			defaultSel.filter(src => src2UrlMap[src]).forEach(src => allSources.push(src));
+		}
 
-	// make a list of src : url objects
-	const toLoads = allSources.map(src => ({src: src, url: jsonDir + src2UrlMap[src]}));
-
-	// TODO promise/then this
-	pageInitFn(loadedSources);
-
-	if (toLoads.length > 0) {
-		DataUtil.multiLoadJSON(
-			toLoads,
-			function (toLoad) {
-				loadedSources[toLoad.src].loaded = true;
-			},
-			function (dataStack) {
-				let toAdd = [];
-				dataStack.forEach(d => toAdd = toAdd.concat(d[dataProp]));
-				addFn(toAdd);
-
-				function finalise () {
-					History.init();
-					handleFilterChange();
-					RollerUtil.addListRollButton();
-					addListShowHide();
-					History.initialLoad = false;
-				}
-
-				if (cbOpt) cbOpt().then(finalise);
-				else finalise();
+		// add source from the current hash, if there is one
+		if (window.location.hash.length) {
+			const [link, ...sub] = History._getHashParts();
+			const src = link.split(HASH_LIST_SEP)[1];
+			const hashSrcs = {};
+			sources.forEach(src => hashSrcs[UrlUtil.encodeForHash(src)] = src);
+			const mapped = hashSrcs[src];
+			if (mapped && $.inArray(mapped, allSources) === -1) {
+				allSources.push(mapped);
 			}
-		);
-	}
+		}
+
+		// make a list of src : url objects
+		const toLoads = allSources.map(src => ({src: src, url: jsonDir + src2UrlMap[src]}));
+
+		pageInitFn(loadedSources);
+
+		if (toLoads.length > 0) {
+			DataUtil.multiLoadJSON(
+				toLoads,
+				(toLoad) => {
+					loadedSources[toLoad.src].loaded = true;
+				},
+				(dataStack) => {
+					let toAdd = [];
+					dataStack.forEach(d => toAdd = toAdd.concat(d[dataProp]));
+					addFn(toAdd);
+
+					const finalise = () => new Promise(resolve => {
+						History.init();
+						handleFilterChange();
+						RollerUtil.addListRollButton();
+						addListShowHide();
+						History.initialLoad = false;
+						resolve();
+					});
+
+					const p = pOptional ? pOptional().then(finalise) : finalise;
+					p.then(resolve);
+				}
+			);
+		} else {
+			resolve();
+		}
+	});
 }
 
 function loadSource (jsonListName, dataFn) {
