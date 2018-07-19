747c6ca907102546ec1d6c9ec40a3936c2e2a5e1
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-07-19 23:05:46 +0100

    fix(history): fix encoded commas
    (cherry picked from commit 5c7584192e23793996ec51eb3c8005aa2397123c)

--- js/history.js
+++ js/history.js
@@ -70,7 +70,7 @@ class History {
 	}
 
 	static _getHashParts () {
-		return window.location.hash.slice(1).split(HASH_PART_SEP);
+		return window.location.hash.slice(1).replace(/%27/g, "'").split(HASH_PART_SEP);
 	}
 
 	static _getListElem (link, getIndex) {
