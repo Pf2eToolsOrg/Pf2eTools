d460630eff12c94aa780cb5156b56280ed20240c
0x0a09942d <63881077+0x0a09942d@users.noreply.github.com>
2020-12-06 19:11:37 +0100

    fix

--- js/render-actions.js
+++ js/render-actions.js
@@ -5,7 +5,7 @@ class RenderActions {
 		${Renderer.utils.getNameDiv(it, {page: UrlUtil.PG_ACTIONS, activity: true, type: ""})}
 		${Renderer.utils.getDividerDiv()}
 		${Renderer.utils.getTraitsDiv(it.traits || [])}
-		${Renderer.feat.getSubHead(it)}
+		${Renderer.action.getSubHead(it)}
 		<div class="pf2-stat-text">
 		${Renderer.get().setFirstSection(true).render({entries: it.entries})}
 		</div>
