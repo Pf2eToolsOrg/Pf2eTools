491abc8ac527af168b1ca81ac1eb8229f6df4084
MrVauxs <mrvauxs@gmail.com>
2021-10-22 21:44:00 +0200

    Licensing Improvements and Sorting

--- js/utils-licenses.js
+++ js/utils-licenses.js
@@ -7,7 +7,7 @@ class UtilsLicenses {
 		$$`${(licenses.map(it => {
 			return `<div class="row"><div class="home__stripe-header text-right home__h-player">
 			<div class="text-center">${it.name != null ? it.name : ""}</div></div>
-			<div>${it.entries.map(e => renderer.render(e)).join("<br>")}</div>
+			<div style="text-align: justify;text-justify: inter-word;">${it.entries.map(e => renderer.render(e)).join("<br>")}</div>
 			</div></div>`
 		}).join("<hr>"))}`.appendTo($wrp);
 	}
