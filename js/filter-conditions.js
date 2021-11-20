61705547b57a830106ab81d8769a8090b39d1ff4
MrVauxs <mrvauxs@gmail.com>
2021-11-20 07:55:12 +0100

    Conditions Page Filters

--- js/filter-conditions.js
+++ js/filter-conditions.js
@@ -1,25 +1,29 @@
 "use strict";
 
 class PageFilterConditions extends PageFilter {
-	// region static
-	static getDisplayProp (prop) {
-		return prop === "status" ? "Other" : prop.uppercaseFirst();
+	constructor () {
+		super();
+
+		this._sourceFilter = new SourceFilter();
+		this._groupFilter = new Filter({header: "Group"})
 	}
-	// endregion
 
 	mutateForFilters (it) {
+		if (it.group) it._fGroup = it.group || "";
 		it._fSources = SourceFilter.getCompleteFilterSources(it);
 	}
 
 	addToFilters (it, isExcluded) {
 		if (isExcluded) return;
 
+		if (it._fGroup != null) this._groupFilter.addItem(it._fGroup);
 		this._sourceFilter.addItem(it._fSources);
 	}
 
 	async _pPopulateBoxOptions (opts) {
 		opts.filters = [
 			this._sourceFilter,
+			this._groupFilter,
 		];
 	}
 
@@ -27,6 +31,7 @@ class PageFilterConditions extends PageFilter {
 		return this._filterBox.toDisplay(
 			values,
 			it._fSources,
+			it._fGroup,
 		)
 	}
 }
