0870d813093d79503f0a504d3afc857c451dccf8
MrVauxs <mrvauxs@gmail.com>
2021-12-07 12:30:48 +0100

    b

--- build/deploy-prep-prod.sh
+++ build/deploy-prep-prod.sh
@@ -19,7 +19,6 @@ echo "Optimizing the header."
 npm run minify:js -- js/styleswitch.js --output js/styleswitch.js
 npm run minify:js -- js/navigation.js --output js/navigation.js
 npm run minify:js -- js/browsercheck.js --output js/browsercheck.js
-# FIXME: Both of these are broken in GitHub Actions, uncomment these if not using a github action
 cat js/styleswitch.js <(echo ";") js/navigation.js <(echo ";") js/browsercheck.js > js/header.js
 rm js/styleswitch.js js/navigation.js
 
@@ -33,7 +32,6 @@ find . -maxdepth 1 -type f -name '*.html' -print0 |
 
 echo "Optimizing the JS."
 # Improve cache performance by gluing these together. Order is important. `echo`s add newlines.
-# FIXME: Both of these are broken in GitHub Actions, uncomment these if not using a github action
 cat js/parser.js <(echo ";") js/utils.js <(echo ";") js/utils-ui.js <(echo ";") js/omnidexer.js <(echo ";") js/omnisearch.js <(echo ";") js/render.js <(echo ";") js/render-dice.js <(echo ";") js/scalecreature.js <(echo ";") js/hist.js > js/shared.js
 rm js/utils.js js/utils-ui.js js/omnidexer.js js/omnisearch.js js/render.js js/render-dice.js js/scalecreature.js js/hist.js
 
