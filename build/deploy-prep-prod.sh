6dbd99d9bcfc9b2080a9bd9c0e489aa8cdc6bb17
MrVauxs <mrvauxs@gmail.com>
2021-12-07 11:32:39 +0100

    Return to default

--- build/deploy-prep-prod.sh
+++ build/deploy-prep-prod.sh
@@ -20,8 +20,8 @@ npm run minify:js -- js/styleswitch.js --output js/styleswitch.js
 npm run minify:js -- js/navigation.js --output js/navigation.js
 npm run minify:js -- js/browsercheck.js --output js/browsercheck.js
 # FIXME: Both of these are broken in GitHub Actions, uncomment these if not using a github action
-# cat js/styleswitch.js <(echo ";") js/navigation.js <(echo ";") js/browsercheck.js > js/header.js
-# rm js/styleswitch.js js/navigation.js
+cat js/styleswitch.js <(echo ";") js/navigation.js <(echo ";") js/browsercheck.js > js/header.js
+rm js/styleswitch.js js/navigation.js
 
 # Replace the files with the minified version we made above
 find . -maxdepth 1 -type f -name '*.html' -print0 |
@@ -31,11 +31,11 @@ find . -maxdepth 1 -type f -name '*.html' -print0 |
 		sed -n -i '/js\/browsercheck.js/!p' $line
     done
 
-# FIXME: Both of these are broken in GitHub Actions, uncomment these if not using a github action
-# echo "Optimizing the JS."
+echo "Optimizing the JS."
 # Improve cache performance by gluing these together. Order is important. `echo`s add newlines.
-# cat js/parser.js <(echo ";") js/utils.js <(echo ";") js/utils-ui.js <(echo ";") js/omnidexer.js <(echo ";") js/omnisearch.js <(echo ";") js/render.js <(echo ";") js/render-dice.js <(echo ";") js/scalecreature.js <(echo ";") js/hist.js > js/shared.js
-# rm js/utils.js js/utils-ui.js js/omnidexer.js js/omnisearch.js js/render.js js/render-dice.js js/scalecreature.js js/hist.js
+# FIXME: Both of these are broken in GitHub Actions, uncomment these if not using a github action
+cat js/parser.js <(echo ";") js/utils.js <(echo ";") js/utils-ui.js <(echo ";") js/omnidexer.js <(echo ";") js/omnisearch.js <(echo ";") js/render.js <(echo ";") js/render-dice.js <(echo ";") js/scalecreature.js <(echo ";") js/hist.js > js/shared.js
+rm js/utils.js js/utils-ui.js js/omnidexer.js js/omnisearch.js js/render.js js/render-dice.js js/scalecreature.js js/hist.js
 
 # Replace the files with the minified version we made above
 find . -maxdepth 1 -type f -name '*.html' -print0 |
