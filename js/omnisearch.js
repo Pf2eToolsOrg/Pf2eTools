b80ff3f0daf191a5c94452bb85f921b94442f5f9
Pf2eTools <78115075+Pf2eTools@users.noreply.github.com>
2022-01-10 17:51:47 +0100

    - FEAT 89: Indexing Table IDs - also index add_hash

--- js/omnisearch.js
+++ js/omnisearch.js
@@ -217,7 +217,7 @@ class Omnisearch {
 
 	static $getResultLink (r) {
 		const href = r.c === Parser.CAT_ID_PAGE ? r.u : `${Renderer.get().baseUrl}${UrlUtil.categoryToPage(r.c)}#${r.uh || r.u}`;
-		return $(`<a href="${href}" ${r.h ? this._renderLink_getHoverString(r.c, r.u, r.s) : ""} class="omni__lnk-name">${r.cf}: ${r.n}</a>`);
+		return $(`<a href="${href}" ${r.h ? this._renderLink_getHoverString(r.c, r.u, r.s) : ""} class="omni__lnk-name">${r.cf}: ${r.n}${r.a ? `<span class="text-muted"> (${r.a.toTitleCase()})</span>` : ""}</a>`);
 	}
 
 	static _pDoSearch_renderLinks (results, page = 0) {
