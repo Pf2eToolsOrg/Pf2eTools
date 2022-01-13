8fbf2e8e7883f7cde92498c297e006face04cc7b
MrVauxs <mrvauxs@gmail.com>
2022-01-14 00:59:35 +0100

    Transition from featType to trait 2/2

--- js/filter-feats.js
+++ js/filter-feats.js
@@ -46,17 +46,12 @@ class PageFilterFeats extends PageFilter {
 		if (!feat._fTraits.map(t => Renderer.trait.isTraitInCategory(t, "Rarity")).some(Boolean)) feat._fTraits.push("Common");
 		feat._fType = [];
 		if (feat.featType == null) feat.featType = {};
-		// TODO: FEAT-90 Transition from featType to trait filter
-		if (feat.featType.class !== false && feat.featType.class != null) {
-			feat._fType.push("Class");
-		}
-		if (feat.featType.ancestry !== false && feat.featType.ancestry != null) {
-			feat._fType.push("Ancestry");
-		}
 
-		if (feat.traits.includes("General")) { feat._fType.push("General"); }
-		if (feat.traits.includes("Skill")) { feat._fType.push("Skill"); }
-		if (feat.traits.includes("Archetype")) { feat._fType.push("Archetype"); }
+		if (feat._fTraits.map(t => Renderer.trait.isTraitInCategory(t, "Ancestry")).some(Boolean)) feat._fType.push("Ancestry");
+		if (feat._fTraits.map(t => Renderer.trait.isTraitInCategory(t, "Class")).some(Boolean)) feat._fType.push("Class");
+		if (feat.traits.includes("General")) feat._fType.push("General");
+		if (feat.traits.includes("Skill")) feat._fType.push("Skill");
+		if (feat.traits.includes("Archetype")) feat._fType.push("Archetype");
 
 		feat._slType = MiscUtil.copy(feat._fType);
 		if (feat._slType.includes("Skill") && feat._slType.includes("General")) feat._slType.splice(feat._slType.indexOf("General"), 1);
