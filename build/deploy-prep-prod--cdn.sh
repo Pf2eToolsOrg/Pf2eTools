a8fba8c1eba1dce12d34214156df6fe5976388b4
Pf2eTools <78115075+Pf2eTools@users.noreply.github.com>
2022-03-23 13:18:33 +0100

    - fixing cdn script

--- build/deploy-prep-prod--cdn.sh
+++ build/deploy-prep-prod--cdn.sh
@@ -2,11 +2,13 @@
 
 echo "Replacing local files with combined jsdelivr."
 
+jquery="3.4.1"
+elasticlunr="0.9"
+
 find . -maxdepth 1 -type f -name '*.html' -print0 |
     while IFS= read -r -d $'\0' line; do
         sed -n -i '/lib\/jquery.js/!p' $line
         sed -n -i '/lib\/elasticlunr.js/!p' $line
-		# Lots of messy escapes below, you care about the version number after the @ sign & the comma.
-        sed -i -e '/<!--PF2ETOOLS_SCRIPT_ANCHOR-->/a <script src="https://cdn.jsdelivr.net/combine/npm/jquery@3.4.1/dist/jquery.min.js,gh/weixsong/elasticlunr.js@0.9/elasticlunr.min.js"><\/script> <script>window.jQuery || document.write(`<script src="/lib\/jquery.js"><\\\/script>`);' $line
+        sed -i -e "\#<!--PF2ETOOLS_SCRIPT_ANCHOR-->#a""<script src='https://cdn.jsdelivr.net/combine/npm/jquery@$jquery/dist/jquery.min.js,gh/weixsong/elasticlunr.js@$elasticlunr/elasticlunr.min.js'></script><script>window.jQuery || document.write('<script src=\'/lib/jquery.js\'></script>');</script>" $line
     done
 
