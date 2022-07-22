d9b80843ce63cbe63fb5a14e3365e63d729b3cce
MrVauxs <mrvauxs@gmail.com>
2022-07-22 23:43:41 +0200

    Update Spells Schema to TC

--- node/update-jsons.js
+++ node/update-jsons.js
@@ -13,11 +13,11 @@ function updateFolder (folder) {
 			let json = ut.readJson(file)
 			// For targeted schema changes, like changing a name of an object key
 			if (json.item) {
-				console.log(`\tUpdating item variants in ${file}...`)
 				json.item = json.item.map(x => {
 					if (x.variants) {
 						x.variants.map(v => {
-							if (v.variantType) return
+							if (v.variantType) return v
+							console.log(`\tUpdating ${x.name} item variants in ${file}...`)
 							v.variantType = v.type
 							delete v.type
 							return v
@@ -27,24 +27,30 @@ function updateFolder (folder) {
 				})
 			}
 			if (json.spell) {
-				console.log(`\tUpdating spell heightening in ${file}...`)
 				json.spell = json.spell.map(x => {
 					if (x.heightened && x.heightened.X && Array.isArray(x.heightened.X)) {
+						console.log(`\tUpdating ${x.name} spell heightening in ${file}...`)
 						let heightenedOld = x.heightened.X
 						x.heightened.X = {}
 						heightenedOld.forEach(v => {
 							x.heightened.X = {...x.heightened.X, [v.level]: v.entries}
 						})
 					}
+					if (x?.range?.type) {
+						console.log(`\tUpdating ${x.name} spell range in ${file}...`)
+						x.range.unit = x.range.type
+						delete x.range.type
+					}
 					return x
 				})
 			}
 			if (json.ancestry) {
-				console.log(`\tUpdating ancestry size in ${file}...`)
 				json.ancestry = json.ancestry.map(x => {
-					if (typeof x.size === 'string' || x.size instanceof String) {
+					if (typeof x.size === "string" || x.size instanceof String) {
+						console.log(`\tUpdating ${x.name} ancestry size in ${file}...`)
 						x.size = x.size.split(/, or |, | or /g)
 					}
+					return x
 				})
 			}
 			console.log(`\tCleaning ${file}...`);
