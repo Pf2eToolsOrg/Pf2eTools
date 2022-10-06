1d6509957ba7f586a8d044bf70614c7256c3633f
MrVauxs <mrvauxs@gmail.com>
2022-10-06 15:07:19 +0200

    sourcing

--- test/scripts/compile-sources-schema.js
+++ test/scripts/compile-sources-schema.js
@@ -4,18 +4,31 @@ const fs = require("fs");
 const ut = require("../../node/util");
 require("../../js/utils");
 
+const schemaTemplate = {
+	$schema: "http://json-schema.org/draft-07/schema#",
+	$id: "sources.json",
+	title: "Sources Schema",
+	description: "A generated file containing a list of all sources in Pf2eTools.",
+	type: "object",
+	version: "0.0.1",
+	definitions: {
+		sourceList: {
+			enum: [],
+		},
+	},
+};
+
 function generatesourceschema (file) {
 	const sourcesFile = ut.readJson("./data/sources.json");
-	let schemaFile = ut.readJson("./test/schema-template/utils.json");
+	let schema = schemaTemplate;
 	const sources = sourcesFile.source;
 
-	// For each language, add it to the schema.
-	const sourceList = sources.map(s => s.source);
-	schemaFile.definitions.sourceList.enum = sourceList.sort();
+	const sourceList = sources.filter(s => !s.unreleased).map(s => s.source);
+	schema.definitions.sourceList.enum = sourceList.sort();
 
 	// Write it all.
-	fs.writeFileSync(file, CleanUtil.getCleanJson(schemaFile), "utf-8");
+	fs.writeFileSync(file, CleanUtil.getCleanJson(schema), "utf-8");
 }
 
-generatesourceschema(`./test/schema-template/utils.json`);
+generatesourceschema(`./test/schema-template/_generated/sources.json`);
 console.log("Sources schema created.");
