8c01491f0eefad3770cd7fde42c693d999220a13
mrvauxs <mrvauxs@gmail.com>
2024-01-09 20:44:44 +0000

    v0.8.8

--- js/filter-spells.js
+++ js/filter-spells.js
@@ -9,12 +9,35 @@ class PageFilterSpells extends PageFilter {
 	// region static
 	static sortSpells (a, b, o) {
 		switch (o.sortBy) {
-			case "name": return SortUtil.compareListNames(a, b);
-			case "source": return SortUtil.ascSort(a.values.source, b.values.source) || SortUtil.compareListNames(a, b);
-			case "level": return SortUtil.ascSort(a.values.level, b.values.level) || SortUtil.compareListNames(a, b);
-			case "type": return SortUtil.ascSort(a.values.type, b.values.type) || SortUtil.compareListNames(a, b);
-			case "school": return SortUtil.ascSort(a.values.school, b.values.school) || SortUtil.compareListNames(a, b);
-			case "time": return SortUtil.ascSort(a.values.normalisedTime, b.values.normalisedTime) || SortUtil.compareListNames(a, b);
+			case "name":
+				return SortUtil.compareListNames(a, b);
+			case "source":
+				return (
+					SortUtil.ascSort(a.values.source, b.values.source)
+					|| SortUtil.compareListNames(a, b)
+				);
+			case "level":
+				return (
+					SortUtil.ascSort(a.values.level, b.values.level)
+					|| SortUtil.compareListNames(a, b)
+				);
+			case "type":
+				return (
+					SortUtil.ascSort(a.values.type, b.values.type)
+					|| SortUtil.compareListNames(a, b)
+				);
+			case "school":
+				return (
+					SortUtil.ascSort(a.values.school, b.values.school)
+					|| SortUtil.compareListNames(a, b)
+				);
+			case "time":
+				return (
+					SortUtil.ascSort(
+						a.values.normalisedTime,
+						b.values.normalisedTime,
+					) || SortUtil.compareListNames(a, b)
+				);
 		}
 	}
 
@@ -29,9 +52,7 @@ class PageFilterSpells extends PageFilter {
 		this._sourceFilter = new SourceFilter();
 		this._levelFilter = new Filter({
 			header: "Level",
-			items: [
-				1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-			],
+			items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 			displayFn: PageFilterSpells.getFltrSpellLevelStr,
 		});
 		this._traditionFilter = new Filter({
@@ -44,7 +65,7 @@ class PageFilterSpells extends PageFilter {
 			items: ["Focus", "Spell", "Cantrip"],
 			itemSortFn: null,
 		});
-		this._classFilter = new Filter({header: "Class"});
+		this._classFilter = new Filter({ header: "Class" });
 		this._subClassFilter = new Filter({
 			header: "Subclass",
 			displayFn: (it) => it.toTitleCase(),
@@ -57,7 +78,11 @@ class PageFilterSpells extends PageFilter {
 		});
 		this._multiFocusFilter = new MultiFilter({
 			header: "Focus Spells",
-			filters: [this._domainFilter, this._classFilter, this._subClassFilter],
+			filters: [
+				this._domainFilter,
+				this._classFilter,
+				this._subClassFilter,
+			],
 		});
 		this._componentsFilter = new Filter({
 			header: "Components",
@@ -75,27 +100,50 @@ class PageFilterSpells extends PageFilter {
 			header: "Duration",
 			isLabelled: true,
 			labelSortFn: null,
-			labels: ["Instant", "1 Round", "1 Minute", "10 Minutes", "1 Hour", "8 Hours", "24+ Hours", "Unlimited", "Special"],
+			labels: [
+				"Instant",
+				"1 Round",
+				"1 Minute",
+				"10 Minutes",
+				"1 Hour",
+				"8 Hours",
+				"24+ Hours",
+				"Unlimited",
+				"Special",
+			],
 		});
 		this._rangeFilter = new RangeFilter({
 			header: "Range",
 			isLabelled: true,
 			labelSortFn: null,
-			labels: ["Touch", "5 feet", "10 feet", "25 feet", "50 feet", "100 feet", "500 feet", "1 mile", "Planetary", "Unlimited", "Varies"],
+			labels: [
+				"Touch",
+				"5 feet",
+				"10 feet",
+				"25 feet",
+				"50 feet",
+				"100 feet",
+				"500 feet",
+				"1 mile",
+				"Planetary",
+				"Unlimited",
+				"Varies",
+			],
 		});
 		this._savingThrowFilter = new Filter({
 			header: "Saving Throw",
 			items: ["Basic", "Fortitude", "Reflex", "Will"],
 		});
-		this._traitFilter = new TraitsFilter({header: "Traits",
+		this._traitFilter = new TraitsFilter({
+			header: "Traits",
 			discardCategories: {
 				Class: true,
-				"School": true,
+				School: true,
 			},
 		});
 		this._schoolFilter = new Filter({
 			header: "School",
-			displayFn: it => it.toTitleCase(),
+			displayFn: (it) => it.toTitleCase(),
 		});
 		this._miscFilter = new Filter({
 			header: "Miscellaneous",
@@ -107,63 +155,118 @@ class PageFilterSpells extends PageFilter {
 		// used for sorting
 		spell._normalisedTime = Parser.getNormalisedTime(spell.cast);
 		spell._normalisedRange = Parser.getNormalisedRange(spell.range);
-		spell._normalisedType = spell.traits.includes("cantrip") && spell.focus ? "FC" : spell.traits.includes("cantrip") ? "C" : spell.focus ? "F" : "S";
+		spell._normalisedType =
+			spell.traits.includes("cantrip") && spell.focus
+				? "FC"
+				: spell.traits.includes("cantrip")
+					? "C"
+					: spell.focus
+						? "F"
+						: "S";
 
 		// used for filtering
 		spell._fSources = SourceFilter.getCompleteFilterSources(spell);
 		spell._fTraditions = (spell.traditions || [])
 			.concat(spell.spellLists || [])
-			.concat(spell.traditions ? spell.traditions.map(x => x.toLowerCase()).includes("primal" || "arcane") ? "Halcyon" : [] : []).map(t => t.toTitleCase());
-		spell._fSpellType = spell.traits.includes("cantrip") && spell.focus ? ["Focus", "Cantrip"] : spell.traits.includes("cantrip") ? ["Cantrip"] : spell.focus ? ["Focus"] : ["Spell"];
-		spell._fTraits = spell.traits.map(t => Parser.getTraitName(t));
-		if (!spell._fTraits.map(t => Renderer.trait.isTraitInCategory(t, "Rarity")).some(Boolean)) spell._fTraits.push("Common");
-		spell._fClasses = spell._fTraits.filter(t => Renderer.trait.isTraitInCategory(t, "Class")) || [];
-		spell._fSubClasses = Object.entries(spell.subclass || {}).map(([k, v]) => {
-			return v.map(sc => {
-				const [subCls, cls] = k.split("|")
-				return new FilterItem({
-					item: sc,
-					nest: `${subCls} (${cls.replace(/\|.+/g, "")})`,
+			.concat(
+				spell.traditions
+					? spell.traditions
+						.map((x) => x.toLowerCase())
+						.includes("primal" || "arcane")
+						? "Halcyon"
+						: []
+					: [],
+			)
+			.map((t) => t.toTitleCase());
+		spell._fSpellType =
+			spell.traits.includes("cantrip") && spell.focus
+				? ["Focus", "Cantrip"]
+				: spell.traits.includes("cantrip")
+					? ["Cantrip"]
+					: spell.focus
+						? ["Focus"]
+						: ["Spell"];
+		spell._fTraits = spell.traits.map((t) => Parser.getTraitName(t));
+		if (
+			!spell._fTraits
+				.map((t) => Renderer.trait.isTraitInCategory(t, "Rarity"))
+				.some(Boolean)
+		) { spell._fTraits.push("Common"); }
+		spell._fClasses =
+			spell._fTraits.filter((t) =>
+				Renderer.trait.isTraitInCategory(t, "Class"),
+			) || [];
+		spell._fSubClasses = Object.entries(spell.subclass || {})
+			.map(([k, v]) => {
+				return v.map((sc) => {
+					const [subCls, cls] = k.split("|");
+					return new FilterItem({
+						item: sc,
+						nest: `${subCls} (${cls.replace(/\|.+/g, "")})`,
+					});
 				});
-			});
-		}).flat();
-		this.handleTraitImplies(spell, { traitProp: "_fTraits", entityTypes: ["spell"] });
+			})
+			.flat();
+		this.handleTraitImplies(spell, {
+			traitProp: "_fTraits",
+			entityTypes: ["spell"],
+		});
 		spell._fSchool = (spell._fSchool || [])[0];
 		spell._fTime = Parser.timeToActivityType(spell.cast);
 		spell._fDurationType = Parser.getFilterDuration(spell);
 		spell._areaTypes = spell.area ? spell.area.types : [];
 		spell._fRange = Parser.getFilterRange(spell);
 		if (spell.savingThrow) {
-			if (spell.savingThrow.type) spell._fSavingThrow = spell.savingThrow.type.map(t => Parser.savingThrowAbvToFull(t))
+			if (spell.savingThrow.type) {
+				spell._fSavingThrow = spell.savingThrow.type.map((t) =>
+					Parser.savingThrowAbvToFull(t),
+				);
+			}
 			if (spell.savingThrow.basic) spell._fSavingThrow.push("Basic");
 		}
 		// TODO: Figure out how to make various configurations of components work in filters
 		// Example: [V], [V and S], [V and S and M]
 		// Right now we'll just live with this.
-		spell._fComponents = [...new Set((spell.components || []).flat())].map(x => Parser.COMPONENTS_TO_FULL[x].toTitleCase());
+		spell._fComponents = [...new Set((spell.components || []).flat())].map(
+			(x) => Parser.COMPONENTS_TO_FULL[x].toTitleCase(),
+		);
 		if (spell.cost != null) spell._fComponents.push("Cost");
 		spell._fMisc = [];
 		if (spell.requirements) spell._fMisc.push("Has Requirements");
 		if (spell.trigger) spell._fMisc.push("Has Trigger");
 		if (spell.targets) spell._fMisc.push("Has Targets");
 		if (spell.heightened) spell._fMisc.push("Can be Heightened");
-		if (spell.duration && spell.duration.sustain) spell._fMisc.push("Sustained");
-		if (spell.duration && spell.duration.dismiss) spell._fMisc.push("Can be Dismissed");
+		if (spell.duration && spell.duration.sustain) { spell._fMisc.push("Sustained"); }
+		if (spell.duration && spell.duration.dismiss) { spell._fMisc.push("Can be Dismissed"); }
 		if (spell.summoning) spell._fMisc.push("Summoning");
 		if (spell.miscTags) {
-			spell.miscTags.forEach(element => {
+			spell.miscTags.forEach((element) => {
 				switch (element) {
-					case "THP": spell._fMisc.push("Grants Temporary Hit Points"); break;
-					case "SM": spell._fMisc.push("Summoning"); break;
-					case "BF": spell._fMisc.push("Has Battle Form"); break;
+					case "THP":
+						spell._fMisc.push("Grants Temporary Hit Points");
+						break;
+					case "SM":
+						spell._fMisc.push("Summoning");
+						break;
+					case "BF":
+						spell._fMisc.push("Has Battle Form");
+						break;
 				}
 			});
 		}
 		// "Possible Elementalist Spell" shenanigans, could be optimised?
-		if (!spell._fTraditions.includes("Elemental")
-		&& (spell.traits.some((trait) => trait.includes("Fire" || "Water" || "Earth" || "Air"))
-		|| (spell._fTraditions.includes("Arcane") && spell._fTraditions.includes("Occult") && spell._fTraditions.includes("Primal") && spell._fTraditions.includes("Divine"))
-		)) spell._fMisc.push("Possible Elementalist Spells");
+		if (
+			!spell._fTraditions.includes("Elemental")
+			&& (spell.traits.some((trait) =>
+				trait.includes("Fire" || "Water" || "Earth" || "Air"),
+			)
+				|| (spell._fTraditions.includes("Arcane")
+					&& spell._fTraditions.includes("Occult")
+					&& spell._fTraditions.includes("Primal")
+					&& spell._fTraditions.includes("Divine")))
+		) { spell._fMisc.push("Possible Elementalist Spells"); }
+		// Remaster hack
+		if (spell.remaster) spell._fMisc.push("Remaster");
 	}
 
 	addToFilters (spell, isExcluded) {
@@ -178,8 +281,8 @@ class PageFilterSpells extends PageFilter {
 		this._classFilter.addItem(spell._fClasses);
 		this._domainFilter.addItem(spell.domains);
 		this._componentsFilter.addItem(spell._fComponents);
-		spell._fSubClasses.forEach(sc => {
-			this._subClassFilter.addNest(sc.nest, {isHidden: true});
+		spell._fSubClasses.forEach((sc) => {
+			this._subClassFilter.addNest(sc.nest, { isHidden: true });
 			this._subClassFilter.addItem(sc);
 		});
 		this._traitFilter.addItem(spell._fTraits);
@@ -221,13 +324,9 @@ class PageFilterSpells extends PageFilter {
 			s._areaTypes,
 			s._fRange,
 			s._fSavingThrow,
-			[
-				s.domains,
-				s._fClasses,
-				s._fSubClasses,
-			],
+			[s.domains, s._fClasses, s._fSubClasses],
 			s._fTraits,
 			s._fMisc,
-		)
+		);
 	}
 }
