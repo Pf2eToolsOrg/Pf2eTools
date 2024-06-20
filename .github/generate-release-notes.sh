8c37d258b9817e6cd05693d12c0972463ee05fcc
Spappz <34202141+Spappz@users.noreply.github.com>
2024-06-20 17:42:15 +0100

    test2

--- .github/generate-release-notes.sh
+++ .github/generate-release-notes.sh
@@ -49,4 +49,7 @@ fi
     changelog=$(sed -E "s/<\/?kbd>/\`/g" <<< "$changelog")
     changelog=$(sed -E "s/<\/?sup>/\"/g" <<< "$changelog")
     jq -r '.txt' <<< "$changelog"
+
+	# Add stagnation notice
+	printf '\n**Note:** Pf2eTools is still stagnated as per [v0.8.5](https://github.com/Pf2eToolsOrg/Pf2eTools/releases/tag/v0.8.5). However, we are making good progress on the new site! An alpha is available to our [supporters](https://ko-fi.com/mrvauxs); you can also join our [Discord server](https://discord.gg/2hzNxErtVu) to receive more informal updates or just chat about things.\n'
 }
